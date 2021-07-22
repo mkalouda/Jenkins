77ed707b4b929f6abb2c9e795dd76b4b45be8a7d
*mkalouda* <munz.kalouda@gmail.com>
2021-07-21 21:10:39 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 9 is great ");
+            Thread.sleep(1000);
+        }
+    }
+}
